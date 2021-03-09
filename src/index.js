130045bd8c74111736325ae56e74c18b4ed2cbe0
Feifeng Liu <liufeifeng@bytedance.com>
2021-03-09 18:31:55 +0800

    add react-final-form and final-form package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/reducer';
